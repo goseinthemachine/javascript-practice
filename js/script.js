cbd7764d84e691fcc0ecbe580440e20953eb45a3
Cameron <cameron.gose@hey.com>
2021-11-03 18:16:19 -0700

    Take home assignment completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+// This is a single line comment
+
+/* 
+This
+Is
+a
+multi-line
+comment 
+*/
+
+console.log("Cameron Gose");
+
+console.log("My favorite food is Lasagna")
